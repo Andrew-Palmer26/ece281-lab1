143dfcc5646ce4cd17b8cf4b8cf609c4ba9713a2
Andrew.Palmer26 <c26andrew.palmer@afacademy.af.edu>
2024-02-11 14:55:01 -0700

    First

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -55,25 +55,36 @@ library ieee;
 -- entity name should match filename, this has been filled out for you  
 entity thirtyOneDayMonth is 
   port(
-	i_A : in std_logic; -- one of four inputs
-	
-	
-	
-						-- output
-  );
+	   i_A : in std_logic;
+       i_B : in std_logic;
+       i_C : in std_logic;
+       i_D : in std_logic;
+       o_Y : out std_logic 
+       );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
-	
+	signal w_sel : std_logic_vector (2 downto 0); -- MUX sel
 	--signals internal to the architecture are declared and initialized such as w_sel
   
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
-	w_sel(0) <= i_C;	-- one
-	--finish assigning signals
+	w_sel(2) <= i_B;
+	w_sel(1) <= i_C;
+	w_sel(0) <= i_D;
 	
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+    o_Y <= i_A when (w_sel = b"000") else
+           NOT i_A when (w_sel = b"001") else
+           i_A when (w_sel = b"010") else
+           NOT i_A when (w_sel = b"011") else
+           i_A when (w_sel = b"100") else
+           NOT i_A when (w_sel = b"101") else
+           i_A when (w_sel = b"110") else
+           NOT i_A when (w_sel = b"111");
+	   
+	
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
